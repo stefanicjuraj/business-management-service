8bdcebf84d1f6b1cfca6c37bf9d793f3cd77333c
stefanicjuraj <jurajstefanic@outlook.com>
2023-11-04 14:59:47 +0100

    feat: add departments get request

--- src/servicelayer/CompanyServices.java
+++ src/servicelayer/CompanyServices.java
@@ -18,4 +18,25 @@ public class CompanyServices {
         this.employeeBusiness = new EmployeeBusiness();
     }
 
+    /**
+     * Maps to HTTP GET requests with the path "/departments" relative to the
+     * base path defined for the class (e.g., "/CompanyServices").
+     *
+     * It expects a query parameter "company" and produces a response in JSON
+     * format.
+     *
+     * URI:
+     * http://localhost:8080/MarasovicKP2/webresources/CompanyServices/departments?company=kxmzgr
+     *
+     * @param companyName the name of the company (your RIT username).
+     * @return the list of all departments for the given company as a JSON
+     *         response.
+     */
+    @GET
+    @Path("departments")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getDepartments(@QueryParam("company") String companyName) {
+        return departmentBusiness.getAll(companyName);
+    }
+
 }
