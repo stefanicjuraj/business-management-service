54317f3d0cbdfab117a5c040b1f85f53931e1390
stefanicjuraj <jurajstefanic@outlook.com>
2023-11-04 15:06:24 +0100

    feat: create employee business class instance

--- src/businesslayer/EmployeeBusiness.java
+++ src/businesslayer/EmployeeBusiness.java
@@ -2,4 +2,13 @@ package businesslayer;
 
 public class EmployeeBusiness extends Business {
 
+    /**
+     * Creates a new instance of the 'EmployeeBusiness' class. It inherits the
+     * initialization of data access and Gson objects from the parent 'Business'
+     * class to maintain consistency across the application's business logic.
+     */
+    public EmployeeBusiness() {
+        super(); // Calls the constructor of the parent class.
+    }
+
 }
