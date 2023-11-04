761a2fe10ff21c93e63bea7a4488ff90990ef50a
stefanicjuraj <jurajstefanic@outlook.com>
2023-11-04 15:07:49 +0100

    feat: add get all employees method

--- src/businesslayer/EmployeeBusiness.java
+++ src/businesslayer/EmployeeBusiness.java
@@ -1,5 +1,7 @@
 package businesslayer;
 
+import java.util.List;
+
 public class EmployeeBusiness extends Business {
 
     /**
@@ -9,6 +11,41 @@ public class EmployeeBusiness extends Business {
      */
     public EmployeeBusiness() {
         super(); // Calls the constructor of the parent class.
+    }
+
+    /**
+     * Retrieves a list of employees for a given company.
+     *
+     * @param companyName the name of the company of which employees are to be
+     *                    retrieved.
+     * @return a JSON response containing a list of employees or an error
+     *         message if no employees are found.
+     */
+    public Response getAll(String companyName) {
+        try {
+            // Retrieve all employees for the given company name
+            List<Employee> employees = this.dl.getAllEmployee(companyName);
+
+            // Check if the employees list is empty or if the company name does not match
+            // the expected value
+            if (employees.isEmpty() || !companyName.equals(BusinessConfig.COMPANY_NAME)) {
+                // Return an OK response with a message indicating no employees were found for
+                // the company
+                return Response.status(Response.Status.OK)
+                        .entity("{\"error\": \"No employees found for the company name provided: " + companyName
+                                + ".\"}")
+                        .build();
+            }
+
+            // Return an OK response with the list of employees
+            return Response.status(Response.Status.OK).entity(employees).build();
+
+        } catch (Exception e) {
+            // Return an Internal Server Error response with a message indicating a database
+            // connection failure
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity("{\"error\":\"Failed to connect to the database: " + e.getMessage() + ".\"}").build();
+        }
     }
 
 }
