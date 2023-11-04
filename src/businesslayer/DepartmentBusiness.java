70beb42f0e9fdd9705ea50e147ba381425b5d5b3
stefanicjuraj <jurajstefanic@outlook.com>
2023-11-04 14:52:08 +0100

    feat: create department business class instance

--- src/businesslayer/DepartmentBusiness.java
+++ src/businesslayer/DepartmentBusiness.java
@@ -2,4 +2,13 @@ package businesslayer;
 
 public class DepartmentBusiness extends Business {
 
+    /**
+     * Creates a new instance of the 'DepartmentBusiness' class. It inherits the
+     * initialization of data access and Gson objects from the parent 'Business'
+     * class to maintain consistency across the application's business logic.
+     */
+    public DepartmentBusiness() {
+        super(); // Calls the constructor of the parent class.
+    }
+
 }
