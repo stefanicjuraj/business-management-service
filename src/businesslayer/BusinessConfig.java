27f8d496c3e14583220af4733ed795a91f68f90c
stefanicjuraj <jurajstefanic@outlook.com>
2023-11-04 14:47:26 +0100

    feat: add default company name constant

--- src/businesslayer/BusinessConfig.java
+++ src/businesslayer/BusinessConfig.java
@@ -2,4 +2,8 @@ package businesslayer;
 
 public class BusinessConfig {
 
+    /**
+     * The constant representing the default company name.
+     */
+    public static final String COMPANY_NAME = "";
 }
