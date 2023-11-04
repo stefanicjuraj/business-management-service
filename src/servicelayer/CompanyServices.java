e642bd4f0cb895837fe041cb86ade5c7bed54427
stefanicjuraj <jurajstefanic@outlook.com>
2023-11-04 14:59:00 +0100

    feat: create company services class instance

--- src/servicelayer/CompanyServices.java
+++ src/servicelayer/CompanyServices.java
@@ -3,4 +3,19 @@ package servicelayer;
 @Path("CompanyServices")
 public class CompanyServices {
 
+    private DepartmentBusiness departmentBusiness = null;
+    private EmployeeBusiness employeeBusiness = null;
+
+    /**
+     * Creates a new instance of CompanyServices.
+     *
+     * It initializes the departmentBusiness and employeeBusiness objects, which
+     * are used to interact with the business logic related to departments and
+     * employees.
+     */
+    public CompanyServices() {
+        this.departmentBusiness = new DepartmentBusiness();
+        this.employeeBusiness = new EmployeeBusiness();
+    }
+
 }
